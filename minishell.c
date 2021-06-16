f95b53a062223de33ee98e987faf76e04d1a265d
RolandGamos <lucasweglarz9@gmail.com>
2021-06-16 22:23:38 +0200

    renamed and normed main.c

--- minishell.c
+++ minishell.c
@@ -1,58 +1,59 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 12:15:48 by lweglarz          #+#    #+#             */
-/*   Updated: 2021/06/16 14:48:36 by lweglarz         ###   ########.fr       */
+/*   Updated: 2021/06/16 22:18:01 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-char    *get_line()
+char	*get_line(void)
 {
-    char *line;
+	char	*line;
 
-    line = NULL;
-    if (line)
-    {
-        free(line);
-        line = NULL;
-    }
-    line = readline("Minishell>");
-    if (line)
-        add_history(line);
-    return (line);
+	line = NULL;
+	if (line)
+	{
+		free(line);
+		line = NULL;
+	}
+	line = readline("Minishell>");
+	if (line)
+		add_history(line);
+	return (line);
 }
 
-void    sig_handler(int signum)
+void	sig_handler(int signum)
 {
-    if (signum == SIGINT)
-    {
-        printf("\n");
-        rl_on_new_line();
-        rl_replace_line("", 0);
-        rl_redisplay();
-    }
+	if (signum == SIGINT)
+	{
+		printf("\n");
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
 }
 
-int main(void)
+int	main(void)
 {
-	t_cmd 	*cmd;
-    char 	*line;
+	t_cmd	*cmd;
+	char	*line;
 
 	cmd = NULL;
-    signal(SIGINT, sig_handler);
-    while (1)
+	signal(SIGINT, sig_handler);
+	while (1)
 	{
-
-        line = get_line();
+		line = get_line();
 		cmd = parse_command(line);
 		fill_cmd_array(line, cmd);
+		parse_cmd_array(cmd);
 		if (cmd)
 			free(cmd);
 	}
+	return (1);
 }
