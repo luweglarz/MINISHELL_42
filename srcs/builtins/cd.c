3dd17be2bdf1bf67137d22d8b05e6d9061e43d87
RolandGamos <lucasweglarz9@gmail.com>
2021-06-30 17:53:14 +0200

    cd done, added .. arg

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,14 +6,42 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 21:52:15 by user42            #+#    #+#             */
-/*   Updated: 2021/06/28 16:29:58 by user42           ###   ########.fr       */
+/*   Updated: 2021/06/30 17:48:46 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
 
+char		*go_back(char *pwd)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (pwd[i])
+	{
+		if (pwd[i] == '/')
+			count = i;
+		i++;
+	}
+	return (ft_substr(pwd, 0, count));
+}
+
 void		builtin_cd(t_cmd cmd, bool pipe)
 {
-	(void)cmd;
+	char		*pwd = NULL;
+	int			size;
+
+	size = 0;
 	(void)pipe;
+	if (ft_strncmp(cmd.arg[1], "..", 2) == 0)
+	{
+		while (getcwd(pwd, size) == NULL)
+			size++;
+		pwd = getcwd(pwd, size);
+		chdir(go_back(pwd));
+	}
+	else
+		chdir(go_back(cmd.arg[1]));
 }
