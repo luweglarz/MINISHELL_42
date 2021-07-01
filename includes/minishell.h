2de6d0927cbd915a8fc427be6368f5cf45c60cff
RolandGamos <lucasweglarz9@gmail.com>
2021-07-01 12:04:36 +0200

    cd donee

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 12:13:53 by lweglarz          #+#    #+#             */
-/*   Updated: 2021/06/28 14:51:58 by lweglarz         ###   ########.fr       */
+/*   Updated: 2021/07/01 11:55:24 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 # include <errno.h>
-#include <string.h>
+# include <string.h>
 
 # include "cmd.h"
 # include "builtin.h"
--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lweglarz <lweglarz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 21:52:15 by user42            #+#    #+#             */
-/*   Updated: 2021/06/30 22:08:25 by user42           ###   ########.fr       */
+/*   Updated: 2021/07/01 12:03:50 by lweglarz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,9 @@ void		builtin_cd(t_cmd cmd, bool pipe)
 
 	size = 0;
 	(void)pipe;
-	if (ft_strncmp(cmd.arg[1], "..", 2) == 0 && cmd.arg[1][2] == '\0')
+	if (ft_strncmp(cmd.arg[1], ".", 1) == 0 && cmd.arg[1][1] == '\0')
+		chdir(getenv("HOME"));
+	else if (ft_strncmp(cmd.arg[1], "..", 2) == 0 && cmd.arg[1][2] == '\0')
 	{
 		while (getcwd(pwd, size) == NULL)
 			size++;
